9da4cca6f78ed721fdd36664be3e499a975f7d36
Darkstar130 <jtmcmic@gmail.com>
2023-06-28 22:47:54 -0500

    Cleanup

--- Javascript/TorpedoVorhaltRechner.js
+++ Javascript/TorpedoVorhaltRechner.js
@@ -2,6 +2,7 @@
 Math.radians = function(degrees)  {
     return degrees * Math.PI / 180;
 };
+
 // Converts from radians to degrees
 Math.degrees = function(radians)  {
     return radians * 180 / Math.PI;
